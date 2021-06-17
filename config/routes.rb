a7f22d987e079696f0374c571cd87c114e26e919
Carlos Alvarado <xavieralvaradom@gmail.com>
2021-06-16 22:19:26 -0400

    tarea finalizada

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "users/login"
   get  "pages/about_us"
+  get "pages/test"
   root "pages#index"
 end
